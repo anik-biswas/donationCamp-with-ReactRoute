1ef671aaa6070a6fdb2618bba4a98c7866c81022
anik-biswas <anik.biswas.an@gmail.com>
2023-09-25 17:55:58 -0700

    Toast massege shows after click danate

--- src/component/donationDetail/DonationDetails.jsx
+++ src/component/donationDetail/DonationDetails.jsx
@@ -1,26 +1,28 @@
 import { useLoaderData, useParams } from "react-router-dom";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const DonationDetails = () => {
     const donations = useLoaderData();
     const {id} = useParams();
     const idInt = parseInt(id);
     const donation = donations.data.find(donation => donation.id === idInt)
     console.log(donation);
-    const {image ,card_color , title_color, text_color, title , category} = donation;
+    const {image ,card_color , title_color, text_color, title  ,description, category, price} = donation;
 
+    const HandleDonate =() => {
+        toast("you have successfully donate");
+    }
     return (
-        <div>
-                <div className=" relative w-full  h-96" style={{backgroundImage: `url(${image})`, backgroundSize : `cover`, backgroundRepeat:`no-repeat`, backgroundPosition : `center`}}>
-                 {/* <div className="  h-20 w-full bg-black bg-opacity-70"></div> */}
-                  {/* <div className="top-11"> */}
-                  {/* <img className=" relative w-full  h-96" src={image} alt="" /> */}
-                    <div className=" absolute bottom-0 py-5 pl-10 h-20 w-full bg-black bg-opacity-70">
-                    <button className="btn btn-primary">Get Started</button>
+        <div className="my-8">
+                <div className="hero min-h-screen relative rounded-xl" style={{backgroundImage: `url(${image})`}}>
+                <div className=" absolute bottom-0 py-5 pl-10 h-24 w-full bg-black bg-opacity-70">
+                    <button className={`btn btn-primary`} onClick={HandleDonate} style={{backgroundColor:text_color}} >Donate ${price}</button>
                     </div>
-                  {/* </div> */}
-                
-                </div>
-         </div>
+                    </div>
+                    <h2 className="text-2xl font-bold mt-5">{title}</h2>
+                    <p className="my-5 text-lg font-semibold text-justify">{description}</p>
+                    <ToastContainer></ToastContainer>
+          </div>
     );
 };
 
